d20bd5edb97d8b487aef318b884c779df6660f52
Paradixe <ParadixeChen@outlook.com>
2023-06-21 02:11:59 +0100

    Add SQLite Sample

---
+++ ResearchHub/Data/ResearchHubContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ResearchHub.Data
+{
+    public sealed class ResearchHubContext : DbContext
+    {
+        public ResearchHubContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ ResearchHub/Model/User.cs
@@ -0,0 +1,18 @@
+﻿namespace ResearchHub
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public RoleType Role { get; set; }
+        public string Organization { get; set; }
+        public string PersonalOrcid { get; set; }
+    }
+
+    public enum RoleType
+    {
+        Researcher,
+        Administrator
+    }
+}

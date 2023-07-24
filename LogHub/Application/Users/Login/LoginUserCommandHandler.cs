9f9f663d31e1230fa7975340b3ed5c009bd26681
Paradixe <57289372+Paradixes@users.noreply.github.com>
2023-07-24 22:22:46 +0100

    Fix Login bug

--- LogHub/Application/Users/Login/LoginUserCommandHandler.cs
+++ LogHub/Application/Users/Login/LoginUserCommandHandler.cs
@@ -1,8 +1,10 @@
 ﻿using LogHub.Application.Abstracts;
 using LogHub.Application.Abstracts.Messaging;
+using LogHub.Domain.Entities.Users;
 using LogHub.Domain.Errors;
 using LogHub.Domain.Repositories;
 using LogHub.Domain.Shared;
+using Microsoft.AspNetCore.Identity;
 
 namespace LogHub.Application.Users.Login;
 
@@ -10,12 +12,17 @@ internal sealed class LoginUserCommandHandler
     : ICommandHandler<LoginUserCommand, string>
 {
     private readonly IJwtProvider _jwtProvider;
+    private readonly IPasswordHasher<User> _passwordHasher;
     private readonly IUserRepository _userRepository;
 
-    public LoginUserCommandHandler(IUserRepository userRepository, IJwtProvider jwtProvider)
+    public LoginUserCommandHandler(
+        IUserRepository userRepository,
+        IPasswordHasher<User> passwordHasher,
+        IJwtProvider jwtProvider)
     {
         _userRepository = userRepository;
         _jwtProvider = jwtProvider;
+        _passwordHasher = passwordHasher;
     }
 
     public async Task<Result<string>> Handle(
@@ -24,6 +31,16 @@ internal sealed class LoginUserCommandHandler
     {
         var user = await _userRepository.GetByEmailAsync(request.Email, cancellationToken);
         if (user is null)
+        {
+            return Result.Failure<string>(
+                DomainErrors.User.InvalidCredentials);
+        }
+
+        var result = _passwordHasher.VerifyHashedPassword(
+            user,
+            user.HashedPassword,
+            request.Password);
+        if (result == PasswordVerificationResult.Failed)
         {
             return Result.Failure<string>(
                 DomainErrors.User.InvalidCredentials);

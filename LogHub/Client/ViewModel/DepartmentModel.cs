e050f3da2b486691e07195279a93506bddc1fc0b
Paradixe <57289372+Paradixes@users.noreply.github.com>
2023-08-04 01:12:33 +0100

    Add Organisation NavMenu

--- LogHub/Client/ViewModel/DepartmentModel.cs
+++ LogHub/Client/ViewModel/DepartmentModel.cs
@@ -3,9 +3,9 @@
 public record DepartmentModel
 {
     public Guid Id { get; set; }
-    public string Name { get; set; }
+    public string Name { get; set; } = string.Empty;
     public string? Description { get; set; }
     public Uri? Logo { get; set; }
-    public Guid ManagerId { get; set; }
+    public Guid ManagerId { get; set; } = Guid.Empty;
     public Guid OrganisationId { get; set; }
 }
--- LogHub/Client/ViewModel/OrganisationModel.cs
+++ LogHub/Client/ViewModel/OrganisationModel.cs
@@ -2,13 +2,15 @@
 
 public record OrganisationModel
 {
-    public Guid OrganisationId { get; set; }
+    public Guid Id { get; set; } = Guid.Empty;
 
-    public Guid ManagerId { get; set; }
+    public Guid ManagerId { get; set; } = Guid.Empty;
 
     public string Name { get; set; } = string.Empty;
 
-    public string Description { get; set; } = string.Empty;
+    public string? Description { get; set; }
 
     public string? Logo { get; set; }
+
+    public Uri? LogoUri { get; set; }
 }

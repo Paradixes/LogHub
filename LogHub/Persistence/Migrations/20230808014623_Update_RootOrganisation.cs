e89351a6097a3c27ca07b3c410d1ceaf89b4e908
Paradixe <57289372+Paradixes@users.noreply.github.com>
2023-08-09 01:00:05 +0100

    Update Sub-Organisation View & Create Sub-Organisation

---
+++ LogHub/Persistence/Migrations/20230808014623_Update_RootOrganisation.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update_RootOrganisation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "RootOrganisationId",
+                table: "Organisations",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Organisations_RootOrganisationId",
+                table: "Organisations",
+                column: "RootOrganisationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Organisations_Organisations_RootOrganisationId",
+                table: "Organisations",
+                column: "RootOrganisationId",
+                principalTable: "Organisations",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Organisations_Organisations_RootOrganisationId",
+                table: "Organisations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Organisations_RootOrganisationId",
+                table: "Organisations");
+
+            migrationBuilder.DropColumn(
+                name: "RootOrganisationId",
+                table: "Organisations");
+        }
+    }
+}
